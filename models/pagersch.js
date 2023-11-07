06e35e1b3802c1334f7080a8b0fcc80241f30ebf
Yura5 <82639298+Yura5@users.noreply.github.com>
2023-11-07 15:15:22 +0100

    pagersch.js

---
+++ models/pagersch.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const pagerSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  
+  pagerplayer: String,
+  pagerid: String,
+  pagertagid: String
+});
+
+module.exports = mongoose.model("Pagers", pagerSchema);
